2f50c9e1c27c3950739f3711f78edb5a2f92bc9e
Nathanael Rardin <nathanaelrardin@gmail.com>
2025-02-26 10:02:09 -0600

    refactor: main and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,156 +1,17 @@
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import "./App.css";
-import useCollectJS from "./hooks/useCollectJS";
+import { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import PaymentForm from "./pages/PaymentForm";
 
-function App() {
-  const { register, handleSubmit, watch } = useForm({
-    defaultValues: {
-      amount: "10.00",
-      first_name: "Jane",
-      last_name: "Doe",
-      email: "someone@example.com",
-      zip: "12345",
-      payment: "creditcard",
-      account_type: "checking",
-    },
-  });
-
-  const paymentMethod = watch("payment");
-
-  useCollectJS({
-    tokenizationKey: "Udt4Wn-N2J4nM-VSTsFK-J6y3QN",
-    config: {
-      paymentSelector: "#payButton",
-      variant: "inline",
-      fields: {
-        ccnumber: {
-          selector: "#ccnumber",
-          placeholder: "0000 0000 0000 0000",
-          enableCardBrandPreviews: true,
-        },
-        ccexp: {
-          selector: "#ccexp",
-          placeholder: "MM / YY",
-        },
-        cvv: {
-          selector: "#cvv",
-          placeholder: "***",
-        },
-        checkaba: {
-          selector: "#checkaba",
-          placeholder: "000000000",
-        },
-        checkaccount: {
-          selector: "#checkaccount",
-          placeholder: "0000000000",
-        },
-        checkname: {
-          selector: "#checkname",
-          placeholder: "Jane Doe",
-        },
-      },
-      fieldsAvailableCallback: () => {
-        console.log("fieldsAvailableCallback");
-      },
-      callback: (response) => {
-        if (response.token) {
-          handleSubmit(submitWithToken(response?.token))();
-        } else {
-          console.error("No token in response");
-        }
-      },
-    },
-  });
-
-  function submitWithToken(token: string) {
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-      const payload = {
-        ...data,
-        payment_token: token,
-      };
-      console.log("submitWithToken payload:", payload);
-    };
-    return onSubmit;
-  }
-
-  return (
-    <div>
-      <h1>Apex V2 + React Demo</h1>
-      <div className="card">
-        <h2>Payment Form Example</h2>
-        <form
-          className="payment-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <div className="form-input">
-            <label>
-              First Name
-              <input type="text" {...register("first_name")} />
-            </label>
-          </div>
-          <div className="form-input">
-            <label>
-              Last Name
-              <input type="text" {...register("last_name")} />
-            </label>
-          </div>
-          <div className="form-input">
-            <label>
-              Email Address
-              <input type="text" {...register("email")} />
-            </label>
-          </div>
-          <div className="form-input">
-            <label>
-              Postal Code
-              <input type="text" {...register("zip")} />
-            </label>
-          </div>
-          <div className="form-input">
-            <label>
-              <input type="radio" value="creditcard" {...register("payment")} />
-              Credit Card
-            </label>
-            <label>
-              <input type="radio" value="check" {...register("payment")} />
-              E-Check
-            </label>
-          </div>
-          {paymentMethod === "creditcard" && (
-            <>
-              <div id="ccnumber">
-                <span>Credit Card Number</span>
-              </div>
-              <div id="ccexp">
-                <span>Expiration Date</span>
-              </div>
-              <div id="cvv">
-                <span>CVV</span>
-              </div>
-            </>
-          )}
-          {paymentMethod === "check" && (
-            <>
-              <div id="checkaba">
-                <span>Routing Number</span>
-              </div>
-              <div id="checkaccount">
-                <span>Account Number</span>
-              </div>
-              <div id="checkname">
-                <span>Account Holder Name</span>
-              </div>
-            </>
-          )}
-          <button id="payButton" type="submit">
-            Pay
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
+const App: FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/form" element={<PaymentForm />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
-import Home from "./pages/Home.tsx";
-import PaymentForm from "./pages/PaymentForm.tsx";
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/form" element={<PaymentForm />} />
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
